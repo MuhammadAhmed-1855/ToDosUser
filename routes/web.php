21f0fec99de553a0abdc35d072af8ecd2c750733
MuhammadAhmed-1855 <i201855@nu.edu.pk>
2023-06-22 09:40:02 +0500

    Updating for Starting Route

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('home');
-// });
+Route::get('/', function () {
+    return view('home');
+});
 
 Route::get('/home', function () {
     return view('home');
